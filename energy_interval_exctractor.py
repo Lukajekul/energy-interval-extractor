a3cf5fe011c307b1a62305cffa9cdb293e6edf9b
Žan Luka Tušar <zanluka.tusar@ngen-group.eu>
2026-07-14 10:59:18 +0200

    Made the warning test less hardcoaded and open for more verison or measurment methods

--- energy_interval_exctractor.py
+++ energy_interval_exctractor.py
@@ -12,6 +12,8 @@ header = ["From", "To", "Measurment", "Value in KWH", "", "MeasurmentPoint:"]
 L1 = 0
 L2 = 0
 
+WARNING_DICTIONARY  = {}
+
 FIRST_RUN = True
 FIRST_PRINT_RUN = True
 
@@ -35,11 +37,12 @@ def read_file(path):
         for row in openingList:
             global L1
             global L2
-            if row[5] == "L1":
-                L1 += 1
-            elif row[5] == "L2":
-                L2 += 1
-          
+            global WARNING_DICTIONARY
+            if row[5] != "L3":
+                if row[5] not in WARNING_DICTIONARY:
+                    WARNING_DICTIONARY[row[5]] = 1
+                else: 
+                    WARNING_DICTIONARY[row[5]] += 1          
             
             valueNumberKWH = float(row[3].replace(".", ""))
             valueNumberKWH /= 1000000
@@ -77,25 +80,43 @@ def read_file(path):
             fillInputList(fromTime, toTime, row[3], valueNumberKWH, openingList)
 
 def fillInputList(fromTime, toTime, measurment, valueNumberKWH, openingList):
-    global FIRST_PRINT_RUN
+    # global FIRST_PRINT_RUN
     global FULL_LIST
-    if FIRST_PRINT_RUN:
-        row = ["From", "To", "MeasurmentHethod", "Value in KWH", "", "MeasumentPoint:", MeasurementPoint]
-        FULL_LIST.append(row)
-        row = [fromTime, toTime, measurment, valueNumberKWH, "", "Direction:", directionValue]
-        FULL_LIST.append(row)
-        FIRST_PRINT_RUN = False
-    else:
-        row = [fromTime, toTime, measurment, valueNumberKWH]
-        FULL_LIST.append(row)
-    if len(FULL_LIST) == len(openingList):
-        write(FULL_LIST)
 
-def write(FULL_LIST):
-    for row in FULL_LIST:
+    row = [fromTime, toTime, measurment, valueNumberKWH]
+    FULL_LIST.append(row)
+
+    #if FIRST_PRINT_RUN:
+    #     row = ["From", , "MeasurmentHethod", "Value in KWH", "", "MeasumentPoint:", MeasurementPoint]
+    #     FULL_LIST.append(row)
+    #     row = [fromTime, toTime, measurment, valueNumberKWH, "", "Direction:", directionValue]
+    #     FULL_LIST.append(row)
+    #     FIRST_PRINT_RUN = False
+    # else:
+    #     row = [fromTime, toTime, measurment, valueNumberKWH]
+    #     FULL_LIST.append(row)
+    if len(FULL_LIST) == len(openingList):
+        sortedList = sorted(FULL_LIST, key=lambda x: x[1])
+        #page.insert_rows(0)
+        page['A1'] = "From"
+        page['B1'] = "To"
+        page['C1'] = "MeasurmentHethod"
+        page['D1'] = "Value in KWH"
+        page['F1'] = "MeasumentPoint:"
+        page['G1'] = MeasurementPoint
+        write(sortedList)
+
+def write(sortedList):
+    warningMessage = "Warning! There have been"
+    lenBegining = len(warningMessage)
+    for i in WARNING_DICTIONARY:
+        warningMessage += f" {WARNING_DICTIONARY[i]} instances of {i},"
+    for row in sortedList:
         page.append(row)
-    if L1 != 0 or L2 != 0:
-        page['F4'] = f"Warning! There have been {L1} instances of L1 and {L2} instance of L2"
+    if len(warningMessage) > lenBegining:
+        page['F4'] = f"{warningMessage[:-1]}"
+    page['F2'] = "Direction:"
+    page['G2'] = directionValue
     workbook.save("../test.xlsx")
 
 def main ():
