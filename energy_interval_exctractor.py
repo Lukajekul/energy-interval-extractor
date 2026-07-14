efe578807a81095917f46b49b63488d865ed9558
Žan Luka Tušar <zanluka.tusar@ngen-group.eu>
2026-07-14 11:54:13 +0200

    Created a way to chose multiple file to read and added the ability to name the output file

--- energy_interval_exctractor.py
+++ energy_interval_exctractor.py
@@ -1,55 +1,42 @@
 from openpyxl import Workbook, load_workbook
 from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
-import csv
 from dateutil import tz
-import xlsxwriter
 from openpyxl.workbook import Workbook
 
-header = ["From", "To", "Measurment", "Value in KWH", "", "MeasurmentPoint:"]
-
-
-L1 = 0
-L2 = 0
-
-WARNING_DICTIONARY  = {}
-
-FIRST_RUN = True
-FIRST_PRINT_RUN = True
-
-FULL_LIST = []
-
-
 workbook = Workbook()
-page = workbook.active
-page.title = "Test"
+
 
 def read_file(path):
+    global page
+    global fullList
+    global warningDistionary
+    global firstRun
+    global MeasurementPoint
+    global directionValue
+    firstRun = True
+    fullList = []
     openingList = []
+    warningDistionary  = {}
     with open(path) as file:
         for line in file:
             splitLine = line.split(";")
             openingList.append(splitLine)
+        page = workbook.create_sheet(title=f"MP_{openingList[1][1][-3:]}")
         openingList = openingList[1:]
         for row in openingList:
-            global L1
-            global L2
-            global WARNING_DICTIONARY
             if row[5] != "L3":
-                if row[5] not in WARNING_DICTIONARY:
-                    WARNING_DICTIONARY[row[5]] = 1
+                if row[5] not in warningDistionary:
+                    warningDistionary[row[5]] = 1
                 else: 
-                    WARNING_DICTIONARY[row[5]] += 1          
+                    warningDistionary[row[5]] += 1          
             
             valueNumberKWH = float(row[3].replace(".", ""))
             valueNumberKWH /= 1000000
 
             row.pop(0)
-            global MeasurementPoint
-            global FIRST_RUN
-            if FIRST_RUN:
+            if firstRun:
                 MeasurementPoint = row[0]
-                FIRST_RUN = False
+                firstRun = False
             row.pop(0)
             
 
@@ -68,7 +55,7 @@ def read_file(path):
             fromTime = datetime.strftime(dateTime, "%Y-%m-%d %H:%M")
             toTime = datetime.strftime(toTime, "%Y-%m-%d %H:%M")
 
-            global directionValue
+            
             if row[2] == 'r':
                 directionValue = "Recieved"
             elif row[2] == "s":
@@ -77,13 +64,11 @@ def read_file(path):
             fillInputList(fromTime, toTime, row[3], valueNumberKWH, openingList)
 
 def fillInputList(fromTime, toTime, measurment, valueNumberKWH, openingList):
-    global FULL_LIST
-
     row = [fromTime, toTime, measurment, valueNumberKWH]
-    FULL_LIST.append(row)
+    fullList.append(row)
 
-    if len(FULL_LIST) == len(openingList):
-        sortedList = sorted(FULL_LIST, key=lambda x: x[1])
+    if len(fullList) == len(openingList):
+        sortedList = sorted(fullList, key=lambda x: x[1])
         page['A1'] = "From"
         page['B1'] = "To"
         page['C1'] = "MeasurmentHethod"
@@ -95,19 +80,32 @@ def fillInputList(fromTime, toTime, measurment, valueNumberKWH, openingList):
 def write(sortedList):
     warningMessage = "Warning! There have been"
     lenBegining = len(warningMessage)
-    for i in WARNING_DICTIONARY:
-        warningMessage += f" {WARNING_DICTIONARY[i]} instances of {i},"
+    for i in warningDistionary:
+        warningMessage += f" {warningDistionary[i]} instances of {i},"
     for row in sortedList:
         page.append(row)
     if len(warningMessage) > lenBegining:
         page['F4'] = f"{warningMessage[:-1]}"
     page['F2'] = "Direction:"
     page['G2'] = directionValue
-    workbook.save("../test.xlsx")
 
 def main ():
-    path = "../readings_quarter_hourly_02072026 071238.csv"
-    read_file(path)
+    listOfPaths = []
+    numberOfFiles = int(input("How many files are you working with? -->"))
+    if numberOfFiles == 1:
+        path = str(input("What is the path to the file? -->"))
+    else: 
+        for i in range(numberOfFiles):
+            path = str(input("What is the path to the file? -->"))
+            listOfPaths.append(path)
+    if numberOfFiles == 1:
+        read_file(path)
+    else:
+        for i in listOfPaths:
+            read_file(i)
+    fileName = str(input("What would you like to name your file? -->"))
+    del workbook["Sheet"]
+    workbook.save(f"../{fileName}.xlsx")
 
 if __name__ == '__main__':
     main()
